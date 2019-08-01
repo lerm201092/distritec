e37cb6b5054c8ade7796a412b204104b1f542379
MoiisesDaviid <mdpolo1995@gmail.com>
2019-07-31 23:17:57 -0500

    conexion

--- modelo/sesion/conexion.php
+++ modelo/sesion/conexion.php
@@ -1,9 +1,18 @@
 <?php
 
+// // DATOS DE CONEXION A LA BASE DE DATOS
+// function conexion() {
+//   $conexion = mysqli_connect("localhost", "root", "", "distritec");  
+//   mysqli_set_charset($conexion,"utf8");
+//   return $conexion;
+// }
+
 // DATOS DE CONEXION A LA BASE DE DATOS
-function conexion() {
-  $conexion = mysqli_connect("localhost", "root", "", "distritec");  
-  mysqli_set_charset($conexion,"utf8");
-  return $conexion;
-}
+ function conexion() {
+   $conexion = mysqli_connect("51.79.98.87", "distrit2_db", "DISTRI*2019", "distrit2_productos");  
+   mysqli_set_charset($conexion,"utf8");
+   return $conexion;
+ }
+
+
 ?>
