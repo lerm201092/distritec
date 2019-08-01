dc230e4e4415dcef54b33cce31a52364c8c0dc43
lerm201092 <lerm201092@gmail.com>
2019-07-31 23:43:33 -0500

    se  adecua conexion php

--- modelo/sesion/conexion.php
+++ modelo/sesion/conexion.php
@@ -1,21 +1,15 @@
 <?php
  
-function conexion() {
- $conexion = mysqli_connect("localhost", "root", "", "distritec");  
-  mysqli_set_charset($conexion,"utf8");
-   return $conexion;
- }
+    function conexion() {
+        $servidor = "LOCAL";    // 1. LOCAL    2. WEB
 
-
-/*
-
- function conexion() {
-   $conexion = mysqli_connect("51.79.98.87", "distrit2_db", "DISTRI*2019", "distrit2_productos");  
-   mysqli_set_charset($conexion,"utf8");
-   return $conexion;
- }
-
-*/
-
-
+        if($servidor == "LOCAL"){
+          $conexion = mysqli_connect("localhost", "root", "", "distritec");  
+        }else{
+          $conexion = mysqli_connect("51.79.98.87", "distrit2_db", "DISTRI*2019", "distrit2_productos"); 
+        }
+        
+        mysqli_set_charset($conexion,"utf8");
+        return $conexion;
+    }
 ?>
