66c52b408a8b88153739bcd644135ce64e9b8f04
lerm201092 <lerm201092@gmail.com>
2019-09-12 00:49:23 -0500

    Se modif. bd

--- Base de datos/tbl_envases.sql
+++ Base de datos/tbl_envases.sql
@@ -1,29 +1,66 @@
-/*
-SQLyog Ultimate v11.11 (64 bit)
-MySQL - 5.5.5-10.4.6-MariaDB 
-*********************************************************************
-*/
-/*!40101 SET NAMES utf8 */;
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generaci√≥n: 12-09-2019 a las 07:45:49
+-- Versi√≥n del servidor: 10.4.6-MariaDB
+-- Versi√≥n de PHP: 7.1.31
 
-create table `envases` (
-	`x®7` varchar (300),
-	`¿®7` varchar (300),
-	`©7` varchar (300),
-	`P©7` varchar (300),
-	`ò©7` varchar (300),
-	`–©7` varchar (300),
-	`™7` varchar (300),
-	`P™7` varchar (300),
-	`ò™7` varchar (300),
-	`‡™7` varchar (300),
-	`(´7` varchar (300),
-	`p´7` varchar (300),
-	`∏´7` varchar (300),
-	`` varchar (300),
-	`8¨7` varchar (300),
-	`Ä¨7` varchar (300),
-	`»¨7` varchar (300),
-	`≠7` varchar (300),
-	`X≠7` varchar (300),
-	`†≠7` varchar (300)
-); 
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `distritec`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `envases`
+--
+
+CREATE TABLE `envases` (
+  `referencia` int(11) NOT NULL,
+  `descripcion` varchar(100) NOT NULL,
+  `accesorios` varchar(100) DEFAULT NULL,
+  `cod_unidad` varchar(100) NOT NULL,
+  `usos` varchar(100) DEFAULT NULL,
+  `capacidad_ml` int(11) DEFAULT NULL,
+  `peso_g` int(11) DEFAULT NULL,
+  `diametro_mm` int(11) DEFAULT NULL,
+  `largo_mm` int(11) DEFAULT NULL,
+  `ancho_mm` int(11) DEFAULT NULL,
+  `diametro_r` int(11) DEFAULT NULL,
+  `altura_r` int(11) DEFAULT NULL,
+  `color` int(11) DEFAULT NULL,
+  `material` int(11) DEFAULT NULL,
+  `precio_und` int(11) DEFAULT NULL,
+  `contenido_pack` int(11) DEFAULT NULL,
+  `precio_pack` int(11) DEFAULT NULL,
+  `cod_forma` int(11) NOT NULL,
+  `cod_linea` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- √çndices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `envases`
+--
+ALTER TABLE `envases`
+  ADD PRIMARY KEY (`referencia`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
