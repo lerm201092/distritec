b95f414d9a5a80916d37e96f1230effd32c7775b
MoiisesDaviid <mdpolo1995@gmail.com>
2019-09-26 15:01:27 -0500

    Update index.php

--- view/index.php
+++ view/index.php
@@ -7,7 +7,7 @@
 <body>
 
     <?php include "./include/header.php"; ?>
-    <?php include "./include/banner.php"; ?>
+    <?php include "./include/slider.php"; ?>  
     <?php include "./include/footer.php"; ?>    
     <?php include "./include/scriptComun.php"; ?>
 </body>
